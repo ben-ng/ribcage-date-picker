7786144814fee4b997c6c9d3a80d54eacdc7a562
Ben Ng <me@benng.me>
2013-12-25 09:13:36 -0800

    Sync to master

---
+++ update.sh
@@ -0,0 +1,9 @@
+git co master -- test/build
+cp -r test/build/* .
+rm -r test
+mv bundle.js bundle.dev.js
+uglifyjs bundle.dev.js > bundle.js
+git commit -a -m "Sync to master"
+git push origin gh-pages
+echo "Done"
+
